b34d663e21d18ad30ec1415cebc3f8fcb1ed17c4
Wildanzr <graita.sukma@gmail.com>
2021-12-24 06:58:48 +0700

    #1 - Initial setup eureka server

---
+++ CrowdAcademy/src/test/java/com/wildannn/crowdacademy/CrowdAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildannn.crowdacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
