b68e8c01f290474585fc3796473968b45ff55f87
Wildanzr <graita.sukma@gmail.com>
2021-12-27 14:01:08 +0700

    #3 - Adding swagger

--- post-stat/src/main/java/com/wildannn/poststat/PostStatApplication.java
+++ post-stat/src/main/java/com/wildannn/poststat/PostStatApplication.java
@@ -2,8 +2,10 @@ package com.wildannn.poststat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PostStatApplication {
 
 	public static void main(String[] args) {
